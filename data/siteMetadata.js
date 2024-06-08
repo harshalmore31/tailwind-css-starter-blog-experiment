72c100fcf2384b56c7ad7cf85c2e62f172975980
harshalmore31 <86048671+harshalmore31@users.noreply.github.com>
2024-06-08 11:07:38 +0530

    edit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Harshal More Blog',
+  author: 'Harshal More',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',

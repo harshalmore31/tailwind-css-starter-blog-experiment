2521f97a67084feaba0c8dbb632968d66ea2b67c
harshalmore31 <86048671+harshalmore31@users.noreply.github.com>
2024-06-08 11:31:54 +0530

    new update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,8 +2,8 @@
 const siteMetadata = {
   title: 'Harshal More Blog',
   author: 'Harshal More',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'DSA blog',
+  description: 'A blog Template modfified by Harshal More',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
